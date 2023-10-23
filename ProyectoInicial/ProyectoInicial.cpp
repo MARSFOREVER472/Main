0ead5c1f44a901c15a3cb5f852ec618bb66f4d5a
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-23 12:39:21 -0300

    Código de C++ compilado y ejecutado sin errores.

--- ProyectoInicial/ProyectoInicial.cpp
+++ ProyectoInicial/ProyectoInicial.cpp
@@ -2,9 +2,29 @@
 //
 
 #include <iostream>
+using namespace std; // Agrega unas dependencias a la clase.
+
+// Llamaremos al método "addNumber()" antes del main().
+
+int addNumber()
+{
+    return 1;
+}
 
 int main()
 {
     std::cout << "Hello World!\na"; // "\n" es un separador de línea.
+    std::string basic_string; // Esto es un "String" básico.
+    std::string second_string; // Éste es un segundo "String".
+
     // Console Out == cout (SUGERENCIA).
+    // Comentar una sola línea.
+
+    /*
+    COMENTARIOS
+    DE VARIAS
+    LÍNEAS
+    */
+
+    addNumber();
 }
