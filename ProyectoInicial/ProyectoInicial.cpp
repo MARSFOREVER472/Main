80685cf375654c68bd4d140ee58a2e8a05b84cac
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-23 12:27:32 -0300

    Comprendiendo significados importantes.

--- ProyectoInicial/ProyectoInicial.cpp
+++ ProyectoInicial/ProyectoInicial.cpp
@@ -5,15 +5,6 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\na"; // "\n" es un separador de línea.
+    // Console Out == cout (SUGERENCIA).
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
